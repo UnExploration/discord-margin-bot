743cc9a3391192b6bae86d75962a692e7827d7f8
Casey <79499541+UnExploration@users.noreply.github.com>
2021-03-22 22:15:14 -0700

    modifed main.py - added make commands and error embdeds

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import re
 import time
 
 import discord
+import asyncio
 import requests
 from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
 listen_channels = [823334410945691709]
-itemList = []
-# 823334410945691709 - Channel ID for main discord 
+
+# 823334410945691709 - Channel item_id for main discord 
 load_dotenv()  
 TOKEN = os.getenv('DISCORD_TOKEN') 
 bot = commands.Bot(command_prefix="!")
@@ -32,8 +33,8 @@ minVolume = 500000 #default volume requirements
 with open('remakeMapping.json','r') as myfile:
         mappingResponse = myfile.read()
 mappingData = json.loads(mappingResponse) 
-# mappingResponse['itemID']['keys'] ItemID is ID of item we want. 
-# Keys are data inside this dictionary 
+# mappingResponse['itemitem_id']['keys'] Itemitem_id is item_id of item we want. 
+# Keys are data insitem_ide this dictionary 
 # examine | members (either True or False) | highalch | lowalch | value | icon | name |
 
 
@@ -64,7 +65,35 @@ async def on_message(ctx):
     await bot.process_commands(message = ctx)
 
 @bot.command(pass_context = True)
-async def vSet(ctx): # set variables like Margin/volume/ 
+async def make(ctx): # set variables like Margin/volume/ 
+    #!make margin 10   ex 
+    #!make volume 4000 ex
+    global minMargin
+    global minVolume
+    splitmsg = ctx.message.content.split()  # [0] is !make
+    valueToChange = str(splitmsg[1]).lower()
+    if splitmsg[2].isnumeric():
+        newNumericValue = int(splitmsg[2])
+        if newNumericValue > 100000000 or newNumericValue < 1:
+            errorEmbed = discord.Embed(color = discord.colour.Color.dark_red())
+            errorEmbed.description = 'new value too large or below 1'
+            errorEmbed.set_author(name = "Error")
+            await ctx.send(embed = errorEmbed)
+    else:
+        return # new value not a number
+    if valueToChange != "margin" and valueToChange != "volume":
+        return # 
+    elif valueToChange == "margin":
+        #changing minMargin
+        minMargin = newNumericValue
+    elif valueToChange == "volume":
+        #changing minVolume   
+        minVolume = newNumericValue
+
+    
+
+
+
     return
 
 @bot.command(pass_context = True)
@@ -90,44 +119,70 @@ async def start(ctx):
     #latestData = latestResponse.json()
     #latestData = latestData['data']
     x = 0
-    for each in latestData:
-        if latestData[each]['high'] is not None and latestData[each]['low'] is not None:
-           if latestData[each]['high'] - latestData[each]['low'] > 40:
+    for item_id in latestData:
+        if latestData[item_id]['high'] is not None and latestData[item_id]['low'] is not None:
+           if latestData[item_id]['high'] - latestData[item_id]['low'] > 40:
                 x += 1
-                if volumeData.get(each) is not None and volumeData.get(each) > 500000 :
-                    print(f'ID:{each} ---- Buy:{latestData[each]["high"]} Sell:{latestData[each]["low"]}---- Volume:{volumeData[each]} ----- Name:{mappingData[each]["name"]}')
+                if volumeData.get(item_id) is not None and volumeData.get(item_id) > 500000 :
+                    print(f'item_id:{item_id} ---- Buy:{latestData[item_id]["high"]} Sell:{latestData[item_id]["low"]}---- Volume:{volumeData[item_id]} ----- Name:{mappingData[item_id]["name"]}')
     print('---------------')
 
 @bot.command(pass_context = True)
-async def create(ctx):
+async def create(ctx): #this creates the intial embed and starts the fresh loop
 
-    global minMargin
-    global minVolume
+    global minMargin; global minVolume; global latestData; global mappingData; global volumeData
+
     priceEmbed = discord.Embed(color = discord.colour.Color.dark_purple())
-    priceEmbed.set_author(name = "OSRS Margin Bot")
+    # priceEmbed.set_author(name = "OSRS Margin Bot")
     await refreshData(ctx) # this should update global variables 
     items   = "" #name
     prices  = "" # buy and sell prices
     margins_volumes = "" #magrin
-    volumes  = "" #volume of item
+    # volumes  = "" #volume of item
+    itemList = []
+    for item_id in latestData:
+        if latestData[item_id]['high'] is not None and latestData[item_id]['low'] is not None:
+            if latestData[item_id]['high'] - latestData[item_id]['low'] > minMargin:
+                currentmargin = latestData[item_id]['high'] - latestData[item_id]['low']
+                if volumeData.get(item_id) is not None and volumeData.get(item_id) > minVolume:
+                    itemList.append({
+                        "item_id": item_id,
+                           "name": mappingData[item_id]['name'],
+                          "limit": mappingData[item_id]['limit'],
+                           "high": latestData[item_id]['high'],
+                            "low": latestData[item_id]['low'],
+                         "margin": currentmargin,
+                         "volume": round(volumeData.get(item_id)/1000)
+                })
+    itemList.sort(key=lambda item:item.get("margin"))
+    itemList.reverse()
+    for each in itemList:
+
+        items += f'{each["name"]} \n'
+        prices += f'B: {each["high"]} -- S: {each["low"]} \n'
+        margins_volumes += f'{each["margin"]} - {each["volume"]}k \n'
+
+
+
+
+
     
-    for each in latestData:
-        if latestData[each]['high'] is not None and latestData[each]['low'] is not None:
-            if latestData[each]['high'] - latestData[each]['low'] > minMargin:
-                currentmargin = latestData[each]['high'] - latestData[each]['low']
-                if volumeData.get(each) is not None and volumeData.get(each) > minVolume:
-                    items   += f'{mappingData[each]["name"]} \n '
-                    prices  += f'Buy:{latestData[each]["high"]} - Sell:{latestData[each]["low"]} \n'
-                    margins_volumes += f'{currentmargin} --- {volumeData[each]}\n'
-                    # volumes  += f'{volumeData[each]} \n '
+    
+    priceEmbed.add_field(name = "Item" , value = items)
+    priceEmbed.add_field(name = "Price", value = prices)
+    priceEmbed.add_field(name = "Margin -- Volume", value = margins_volumes)
+    priceEmbed.set_footer(text = f'Min Margin : {minMargin} Min Volume : {minVolume /1000}k')
+    if len(priceEmbed) > 6000: # work around for two tables? #check for length and sent two embeds? 
+        errorEmbed = discord.Embed(color = discord.colour.Color.dark_red())
+        errorEmbed.description = f'Embed exceeds 6000 Character limit. Please adjust settings. \n Current Minimum Margin : {minMargin} \n Current Minimum Volume : {minVolume}'
+        errorEmbed.set_author(name = "Error")
+        await ctx.send(embed = errorEmbed)
+        return
+    else:
+        await ctx.send(embed = priceEmbed)
+    # sort by limit * margin?
 
-    priceEmbed.add_field(name = "Items" , value = items)
-    priceEmbed.add_field(name = "Prices", value = prices)
-    priceEmbed.add_field(name = "Margin --- Volume", value = margins_volumes)
-    priceEmbed.set_footer(text = f'Min Margin : {minMargin} Min Volume : {minVolume}')
-    # priceEmbed.add_field(name = "Volume", value = volumes, inline = True)
-
-    await ctx.send(embed = priceEmbed)
+    
 
 
 
