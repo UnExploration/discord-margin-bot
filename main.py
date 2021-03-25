2ef4fbd7b2a2c2bc82b64e9a66cf53f496670a70
UnExploration <79499541+UnExploration@users.noreply.github.com>
2021-03-24 23:37:55 -0700

    added update loop in main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import requests
 from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
+from datetime import datetime
 
 listen_channels = [823334410945691709]
 
@@ -19,16 +20,20 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') 
 bot = commands.Bot(command_prefix="!")
 
-# Steps
-# Aqquire Data - Refresh 1 min *
-# Sort Data 
-# Put Data in table
-# Notification? 
-# Future
-refreshDelay = 30 # in seconds as this is for asyncio.sleep
+
+# delete !command messages
+# maybe move embed into object check for existing object in channel before making new object.
+# last update being red to signify stopped. 
+
+refreshDelay = 10 # in seconds as this is for asyncio.sleep
 stopRefresh = False
-minMargin = 40 #default margin requirments
-minVolume = 500000 #default volume requirements
+minMargin = 40 #default margin requirments that is used to generate table
+minVolume = 500000 #default volume requirements that is used to generate table
+minRefreshTime = 1 #time in second for the embed refresh
+maxRefreshTime = 300
+
+minNumberValue = 1
+maxNumberValue = 1000000000
 
 # load the mapping file here so we only have to load it once
 with open('remakeMapping.json','r') as myfile:
@@ -37,7 +42,18 @@ mappingData = json.loads(mappingResponse)
 # mappingResponse['itemitem_id']['keys'] Itemitem_id is item_id of item we want. 
 # Keys are data insitem_ide this dictionary 
 # examine | members (either True or False) | highalch | lowalch | value | icon | name |
+errorlist = [
+    f'Embed exceeds 6000 Character limit. Please adjust settings. \n Current Minimum Margin : {minMargin} \n Current Minimum Volume : {minVolume}',
+    f'Syntax -- !make <margin|volume|refreshtime> <int> --',
+    f'Current minimum margin is {minNumberValue}',
+    f'Current minimum and maximum volume is {minNumberValue} and {maxNumberValue}',
+    f'Current minimum and maximum refresh time is {minRefreshTime} and {maxRefreshTime}'
 
+
+
+
+
+]
 
 volumeUrl = 'https://prices.runescape.wiki/api/v1/osrs/volumes'
 latestUrl = 'https://prices.runescape.wiki/api/v1/osrs/latest'  #requested User Agent for the API
@@ -66,36 +82,51 @@ async def on_message(ctx):
     await bot.process_commands(message = ctx)
 
 @bot.command(pass_context = True)
+async def stop(ctx):
+    global stopRefresh
+    stopRefresh = True
+
+
+
+@bot.command(pass_context = True)
 async def make(ctx): # set variables like Margin/volume/ 
     #!make margin 10   ex 
     #!make volume 4000 ex
-    global minMargin
-    global minVolume
+    global minMargin; global minVolume; global refreshDelay; global minResfeshTime; global maxRefreshTime; global minNumberValue; global maxNumberValue
     splitmsg = ctx.message.content.split()  # [0] is !make
     valueToChange = str(splitmsg[1]).lower()
     if splitmsg[2].isnumeric():
         newNumericValue = int(splitmsg[2])
-        if newNumericValue > 100000000 or newNumericValue < 1:
-            errorEmbed = discord.Embed(color = discord.colour.Color.dark_red())
-            errorEmbed.description = 'new value too large or below 1'
-            errorEmbed.set_author(name = "Error")
-            await ctx.send(embed = errorEmbed)
     else:
+
+        await sendErrorEmbed(ctx,1)
+
         return # new value not a number
     if valueToChange != "margin" and valueToChange != "volume" and valueToChange != "refreshtime" and valueToChange != "refresh":
+
+        await sendErrorEmbed(ctx,1)
         return # 
     elif valueToChange == "margin":
-        #changing minMargin
-        minMargin = newNumericValue
+        if newNumericValue < minNumberValue:
+
+            await sendErrorEmbed(ctx,2)
+        else:
+            minMargin = newNumericValue
     elif valueToChange == "volume":
-        #changing minVolume   
-        minVolume = newNumericValue
+        if newNumericValue > maxNumberValue or newNumericValue < minNumberValue:
+            await sendErrorEmbed(ctx,3)
+        else: 
+            minVolume = newNumericValue
     elif valueToChange == "refreshtime":# #default 30
-        return
+
+        if newNumericValue > maxRefreshTime or newNumericValue < minRefreshTime:
+            await sendErrorEmbed(ctx,4)
+
+        else:
+            newNumericValue = refreshDelay
+            
         #make a min and max refresh time.     
-    elif valueToChange == "refresh":
-        return
-        #turn refresh on or off
+   
     return
 
 @bot.command()
@@ -146,7 +177,11 @@ async def create(ctx): #this creates the intial embed and starts the fresh loop
         items += f'{each["name"]} \n'
         prices += f'B: {each["high"]} -- S: {each["low"]} \n'
         margins_volumes += f'{each["margin"]} - {each["volume"]}k \n'
-
+    
+    if prices == "":
+        prices          += '------'
+        items           += '------'
+        margins_volumes += '---------------------'
 
 
 
@@ -158,16 +193,13 @@ async def create(ctx): #this creates the intial embed and starts the fresh loop
     priceEmbed.add_field(name = "Margin -- Volume", value = margins_volumes)
     priceEmbed.set_footer(text = f'Min Margin : {minMargin} Min Volume : {minVolume /1000}k')
 
-    
+    global errorlist
     if len(priceEmbed) > 6000: # work around for two tables? #check for length and sent two embeds? 
-        errorEmbed = discord.Embed(color = discord.colour.Color.dark_red())
-        errorEmbed.description = f'Embed exceeds 6000 Character limit. Please adjust settings. \n Current Minimum Margin : {minMargin} \n Current Minimum Volume : {minVolume}'
-        errorEmbed.set_author(name = "Error")
-        await ctx.send(embed = errorEmbed)
+        await sendErrorEmbed(ctx,0)
         return
     else:
-        await ctx.send(embed = priceEmbed)
-        
+        embedToEdit = await ctx.send(embed = priceEmbed)
+        asyncio.create_task(startUpdateLoop(embedToEdit))
     # sort by limit * margin?
     
 
@@ -178,6 +210,13 @@ async def startUpdateLoop(embedToEdit):
     while stopRefresh == False:
         await asyncio.sleep(refreshDelay)
         await updateEmbed(embedToEdit)
+
+async def sendErrorEmbed(ctx, error): #error is a number that correlates to an entry in the error list
+    global errorlist
+    errorEmbed = discord.Embed(color = discord.colour.Color.dark_red())
+    errorEmbed.description = errorlist[error]
+    await ctx.send(embed = errorEmbed)
+
 
 
 async def updateEmbed(embedToUpdate):
@@ -215,11 +254,27 @@ async def updateEmbed(embedToUpdate):
         items += f'{each["name"]} \n'
         prices += f'B: {each["high"]} -- S: {each["low"]} \n'
         margins_volumes += f'{each["margin"]} - {each["volume"]}k \n'
+    
+    if prices == "":
+        prices          += '------'
+        items           += '------'
+        margins_volumes += '---------------------'
+
+    newEmbed.add_field(name = "Item" , value = items)
+    newEmbed.add_field(name = "Price", value = prices)
+    newEmbed.add_field(name = "Margin -- Volume", value = margins_volumes)
+    newEmbed.set_footer(text = f'Min Margin : {minMargin} Min Volume : {minVolume /1000}k   Time : {datetime.now().strftime("%H:%M:%S")}  ')
 
 
 
+    global errorlist 
+
+    # no check for embed over 6000 ******
 
     await embedToUpdate.edit(embed = newEmbed)
+
+
+    
 
 
 bot.run(TOKEN)
